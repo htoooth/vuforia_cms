a31915035cef3d06743725fcc1ade7cd0640aacb
tengyan <test@example.me>
2015-01-28 20:47:54 +0900

    add コンテン一覧を作成。

--- cms/views/content.py
+++ cms/views/content.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.db.models.query import Q
+
 from cms.models import (UserProfile, AdminUser, AgencyUser, CompanyUser,
                         Content)
 
+@login_required
 def list(request):
-    # 当該ユーザーの持つ全コンテンツを渡す。
-    # とりあえず Company
-    cid = 1
-    contents_list = Content.object.filter(user_id__exact=cid, contract_no=c_no)
+    # ログインしているユーザー所有のアカウントの持つ全コンテンツを渡す。
+    if request.user.acc_type_id == 1:
+        content_list = Content.objects.filter(
+            company__acc_type_id__exact=3,
+            company__parent_admin_id__exact=request.user.user_id
+        )
+    elif request.user.acc_type_id == 2:
+        content_list = Content.objects.filter(
+            company__acc_type_id__exact=3,
+            company__parent_agency_id__exact=request.user.user_id
+        )
+    elif request.user.acc_type_id == 3:
+        content_list = Content.objects.filter(
+            company__user_id__exact=request.user.user_id
+        )
 
-    return render(request, 'content_list.html', {'content_list': contents_list})
+    return render(request, 'content_list.html',
+                  {'content_list': content_list})
 
