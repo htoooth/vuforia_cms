b84f6ecc73c9ee7e6ea3bca168676a1b5c14ee99
tengyan <test@example.me>
2015-02-18 18:59:26 +0900

    add カスタムパスワード変更フォームを作成。
    8文字以上で、英数字を織り交ぜることを要求する。

--- cms/views/account.py
+++ cms/views/account.py
@@ -157,7 +157,7 @@ def edit_pw(request, acctypeid, userid):
     if request.POST:
         i = UserProfile.objects.get(acc_type_id=acctypeid,
                                     user_id=userid)
-        form = PasswordChangeForm(user=i, data=request.POST)
+        form = ValidatingPasswordChangeForm(user=i, data=request.POST)
         #form = UserProfilePasswordForm(request.POST, instance=i)
         if form.is_valid():
             #password = form.cleaned_data['password']
@@ -181,7 +181,7 @@ def edit_pw(request, acctypeid, userid):
                            'acctypeid': int(acctypeid),
                            'userid': int(userid)})
     else:
-        form = PasswordChangeForm(
+        form = ValidatingPasswordChangeForm(
             user=UserProfile.objects.get(acc_type_id=acctypeid,
                                              user_id=userid)
         )
@@ -196,3 +196,23 @@ class UserProfileForm(forms.ModelForm):
         fields = ('acc_type_id', 'enterprise', 'person', 'address',
                   'email','phone_number',)
 
+class ValidatingPasswordChangeForm(PasswordChangeForm):
+    MIN_LENGTH = 8
+
+    def clean_new_password1(self):
+        password1 = self.cleaned_data.get('new_password1')
+
+        # At least MIN_LENGTH long
+        if len(password1) < self.MIN_LENGTH:
+            raise forms.ValidationError(
+                "The new password must be at least %d characters long." % self.MIN_LENGTH)
+
+        # At least one letter and one non-letter
+        first_isalpha = password1[0].isalpha()
+        if all(c.isalpha() == first_isalpha for c in password1):
+            raise forms.ValidationError(
+                "The new password must contain at least one letter and at least one digit or punctuation character.")
+
+        # ... any other validation you want ...
+
+        return password1
